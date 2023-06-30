05828794e4580d93a3a574c64825a284e24628c2
Kevin <kevchi@users.noreply.github.com>
2023-06-30 03:56:03 -0500

    updates

--- js/dice-game.js
+++ js/dice-game.js
@@ -4,14 +4,14 @@ function rollDice() {
     const roll = Math.floor(Math.random() * 6) + 1;
     alert(`You rolled a ${roll}!`);
     if (roll === 1) {
-      alert("Game over, no more rolls!");
+      alert('Game over, no more rolls!');
       break;
     }
     if (roll < 5) {
       continue;
     }
     if (roll === 4 && goldCoins > 0) {
-      alert("Sorry, you lose 1 gold coin!");
+      alert('Sorry, you lose 1 gold coin!');
       goldCoins -= 1;
     } else {
       alert(`Congratulations, you win ${roll} gold coins!`);
